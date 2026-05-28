32fec40ec64c87bc4f6594bc9e8f76f9b1935f0a
Luan Ferrite <luan.ferrite@ldv.local>
2026-05-28 16:55:27 -0300

    remove supabase key

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = 'https://nzknbxijgfwrevjnkkrt.supabase.co';
-
-const SUPABASE_KEY = 'sb_publishable_wokxfC6LdHQ730feDu1eJw_DM1zOcw_';
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
