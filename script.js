409a88851cba15473b4d8fd7ab9e1cf5d1967666
Luan Ferrite <luan.ferrite@ldv.local>
2026-05-28 16:54:09 -0300

    remove secret key

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ async function loadMatches() {
 
     const { data, error } = await supabaseClient
         .from('matches')
-        .select('*')
-        .order('match_date');
+        .select('*');
+
+    console.log(data);
 
     if (error) {
         console.error(error);
@@ -12,60 +13,18 @@ async function loadMatches() {
 
     const container = document.getElementById('matches-container');
 
-    container.innerHTML = '';
-
     data.forEach(match => {
 
-        const card = document.createElement('div');
+        container.innerHTML += `
+            <div class="match-card">
 
-        card.classList.add('match-card');
+                <h2>
+                    ${match.team_home} vs ${match.team_away}
+                </h2>
 
-        card.innerHTML = `
-            <div class="teams">
-                <span>${match.team_home}</span>
-                <span>vs</span>
-                <span>${match.team_away}</span>
             </div>
-
-            <div class="inputs">
-                <input type="number" id="home-${match.id}">
-                <input type="number" id="away-${match.id}">
-            </div>
-
-            <button onclick="savePrediction(${match.id})">
-                Salvar Palpite
-            </button>
         `;
-
-        container.appendChild(card);
     });
-}
-
-async function savePrediction(matchId) {
-
-    const home = document.getElementById(`home-${matchId}`).value;
-    const away = document.getElementById(`away-${matchId}`).value;
-
-    const userId = localStorage.getItem('user_id');
-
-    const { error } = await supabaseClient
-        .from('predictions')
-        .insert([
-            {
-                user_id: userId,
-                match_id: matchId,
-                prediction_home: home,
-                prediction_away: away
-            }
-        ]);
-
-    if (error) {
-        alert('Erro ao salvar palpite');
-        console.error(error);
-        return;
-    }
-
-    alert('Palpite salvo!');
 }
 
 loadMatches();
--- supabase.js
+++ supabase.js
@@ -1,4 +1,5 @@
-const SUPABASE_URL = 'https://nzknbxijgfwrevjnkkrt.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://nzknbxijgfwrevjnkkrt.supabase.co';
+
 const SUPABASE_KEY = 'sb_publishable_wokxfC6LdHQ730feDu1eJw_DM1zOcw_';
 
 const supabaseClient = supabase.createClient(
