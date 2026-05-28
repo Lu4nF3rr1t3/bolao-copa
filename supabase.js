1d05da14184bec7356fb5b6c284bd7175169f65b
Luan Ferrite <luan.ferrite@ldv.local>
2026-05-28 17:10:45 -0300

    corrige import supabase

--- supabase.js
+++ supabase.js
@@ -1,8 +1,10 @@
+const { createClient } = supabase;
+
 const SUPABASE_URL = 'https://nzknbxijgfwrevjnkkrt.supabase.co';
 
 const SUPABASE_KEY = 'sb_publishable_wokxfC6LdHQ730feDu1eJw_DM1zOcw_';
 
-const supabaseClient = supabase.createClient(
+const supabaseClient = createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
